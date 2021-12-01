a8043b962753e4bf3f5620d313408940e0aec8c4
Ian D <43609709+haharkagain@users.noreply.github.com>
2021-12-01 12:52:14 -0600

    Add files via upload

---
+++ main/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <mylib.h>
+
+int main() {
+	std::cout << print_hello() << std::endl;
+	return 0;
+}
---
+++ mylib/mylib.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "mylib.h"
+
+int print_hello() {
+	std::cout << "hello world" << std::endl;
+	return 7;
+}
